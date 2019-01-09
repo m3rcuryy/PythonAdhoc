93b12db60b11fc62897786d316b536578d9beb5a
root <root@station119example.com>
2019-01-09 11:20:36 +0530

    added fb.py

---
+++ fb.py
@@ -0,0 +1,28 @@
+from selenium import webdriver 
+from time import sleep 
+
+usr="himanshugrg151@gmail.com"
+pwd="check"
+
+driver = webdriver.Firefox() 
+driver.get('https://www.facebook.com/') 
+print ("Opened facebook") 
+sleep(1) 
+
+username_box = driver.find_element_by_id('email') 
+username_box.send_keys(usr) 
+print ("Email Id entered") 
+sleep(1) 
+
+password_box = driver.find_element_by_id('pass') 
+password_box.send_keys(pwd) 
+print ("Password entered") 
+
+login_box = driver.find_element_by_id('loginbutton') 
+login_box.click() 
+
+print ("Done") 
+raw_input('Press anything to quit') 
+driver.quit() 
+print("Finished") 
+
